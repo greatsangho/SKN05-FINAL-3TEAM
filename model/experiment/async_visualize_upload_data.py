e4d6e3b18b83619aba482d1720c6c2d25c10e308
MartinusChoi <martinus.choi@gmail.com>
2025-02-05 10:05:13 +0900

    async visualize upload data process

--- model/experiment/async_visualize_upload_data.py
+++ model/experiment/async_visualize_upload_data.py
@@ -1,13 +1,10 @@
 import os
-from config.secret_keys import OPENAI_API_KEY, LANGSMITH_API_KEY, LANGSMITH_ENDPOINT, LANGSMITH_PROJECT, LANGSMITH_TRACING, TAVILY_API_KEY, USER_AGENT
+from config.secret_keys import OPENAI_API_KEY, LANGSMITH_API_KEY, LANGSMITH_ENDPOINT, LANGSMITH_PROJECT, LANGSMITH_TRACING
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 os.environ["LANGSMITH_API_KEY"] = LANGSMITH_API_KEY
 os.environ["LANGSMITH_ENDPOINT"] = LANGSMITH_ENDPOINT
 os.environ["LANGSMITH_PROJECT"] = LANGSMITH_PROJECT
 os.environ["LANGSMITH_TRACING"] = LANGSMITH_TRACING
-os.environ["TAVILY_API_KEY"] = TAVILY_API_KEY
-os.environ["USER_AGENT"] = USER_AGENT
-
 
 from typing_extensions import TypedDict
 from typing import Annotated, List
@@ -16,7 +13,7 @@ from langchain_core.messages import BaseMessage
 from langchain_core.documents import Document
 
 from langgraph.graph import StateGraph, START, END
-from finpilot.experimental.visualize_web_data import VisualizeWebDataProcess
+from finpilot.experimental.visualize_upload_data import VisualizeUploadDataProcess
 
 import uvicorn
 from fastapi import FastAPI
@@ -35,26 +32,24 @@ class State(TypedDict):
     source : List[str]
 
 
-
-visualize_web_data_process = VisualizeWebDataProcess(session_id="session_tmp")
+visualize_upload_data_process = VisualizeUploadDataProcess(session_id="session_tmp")
 
 workflow = StateGraph(State)
 
-workflow.add_node("visualize_node", visualize_web_data_process.visualize_node)
-workflow.add_node("tool_node", visualize_web_data_process.tool_node)
+workflow.add_node("visualize_node", visualize_upload_data_process.visualize_node)
 
 workflow.add_edge(START, "visualize_node")
 workflow.add_conditional_edges(
     "visualize_node",
-    visualize_web_data_process.should_continue,
+    visualize_upload_data_process.should_continue,
     {
-        "continue" : "tool_node",
+        "continue" : "visualize_node",
         "end" : END
     }
 )
-workflow.add_edge("tool_node", "visualize_node")
 
 app = workflow.compile()
+
 
 
 
@@ -70,15 +65,13 @@ async def query():
     data_path = Path(os.getcwd()) / "data" / f"session_tmp"
     if not os.path.exists(data_path):
         os.makedirs(data_path)
-
-    # invoke answer
+    
     print("[Server Log] INVOKING PILOT ANSWER (IMAGE)")
     answer = await app.ainvoke(
-        {"question" : "최근 5개년 미국 GDP 그래프를 그려줘"}
+        {"question" : "주어진 데이터를 다양한 시각에서 분석하여 시각화해줘줘"}
     )
     print("[Server Log] PILOT ANSWER INVOKED")
 
-    # Get PNG File list
     png_files = [f for f in os.listdir(folder_path) if f.endswith(".png")]
     if not png_files:
         raise HTTPException(status_code=404, detail="No PNG files found in the folder")
@@ -92,6 +85,5 @@ async def query():
 
 
 
-
 if __name__ == "__main__":
-    uvicorn.run("async_visualize_web_data:server", host="localhost", reload=True)
+    uvicorn.run("async_visualize_upload_data:server", host="localhost", reload=True)
